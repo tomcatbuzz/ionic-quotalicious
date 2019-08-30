7de5b440e30f6fe43475f56386d5d39f4962afc2
Tony <tomcatbuzz@yahoo.com>
2019-08-30 19:56:50 -0400

    pre-build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'favorites', pathMatch: 'full' },
+  { path: 'favorites', loadChildren: () => import('./favorites/favorites.module').then( m => m.FavoritesPageModule)},
+  { path: 'library', loadChildren: './library/library.module#LibraryPageModule' },
+  { path: 'quotes', loadChildren: './quotes/quotes.module#QuotesPageModule' },
+  { path: 'quote', loadChildren: './quote/quote.module#QuotePageModule' },
+  { path: 'settings', loadChildren: './settings/settings.module#SettingsPageModule' },
 ];
 
 @NgModule({
